dbf4497854b9be636798179a3a2e0de39fc8d79e
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-04-23 02:22:56 -0700

    :art: modified labels

--- 2020/16_rap_songs/16_rap_artists.R
+++ 2020/16_rap_songs/16_rap_artists.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(ggrepel)
 library(extrafont)
 library(Cairo)
+library(gganimate)
 
 fonttable <- fonttable()
 #font_import(paths = c("c:/Users/isabe/FontBase"))
@@ -13,11 +14,11 @@ loadfonts(device = "win", quiet = TRUE) ## to load the font
 # Set theme
 
 theme_set(theme_minimal())
-theme <- theme_update(text = element_text(family = "Encode Sans Thin", size = 13),
+theme <- theme_update(text = element_text(family = "Source Sans Pro Light", size = 13),
                       title = element_text(family = "Fira Mono", size = 20, color = "gray20"),
                       plot.title = element_text(family = "CHANEY Ultra Extended", size = 20, color = "gray20"),
                       plot.subtitle = element_text(family = "Gruppo", size = 25, color = "gray20"),
-                      plot.caption = element_text(family = "Encode Sans Thin", size = 12, color = "gray20"),
+                      plot.caption = element_text(family = "Source Sans Pro Light", size = 13, color = "gray20"),
                       legend.text = element_text("Gruppo", size = 16),
                       axis.text = element_text(size = 14),
                       axis.line.x = element_line(color = "gray80"),
@@ -36,6 +37,10 @@ rankings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/t
 top <- rankings %>% 
   head(6)
 
+top_females <- rankings %>%
+  filter(gender  == "female") %>% 
+  head(2)
+
 notable <- rankings %>% 
   filter(year >= 2010 | year == min(year)) %>% 
   head(3)
@@ -46,9 +51,10 @@ labels <- rbind(top,notable) %>%
 rankings %>% 
   ggplot(aes(x = year, y = points)) +
   geom_point(aes(color = gender),size = 4, alpha = 0.3) +
+  geom_point(data = top_females, aes(x = year, y = points), color = "#f35588",size = 4, alpha = 0.3) +
   labs(title = "Top Hip Hop Songs",
        subtitle = "According to 107 critics worldwide",
-       caption = "Critic rating: Each critic voted for five songs, ranking them from one (favourite) to five (5th favourite).\nBBC Music awarded 10 points for first ranked track, eight points for second ranked track, \nand so on down to two points for fifth place.The song with the most points won.\nSource: BBC Music (2019)",
+       caption = "Critic rating: Each critic voted for five songs, ranking them from one (favourite) to five (5th favourite). BBC Music awarded 10 points for first ranked track,\neight points for second ranked track, and so on down to two points for fifth place.The song with the most points won.\nSource: BBC Music (2019)",
        x = "",
        y = "Critic Rating",
        color = ""
@@ -64,13 +70,14 @@ rankings %>%
   ) +
   geom_label_repel(data = labels, 
                    aes(label = label),
-                   family = "Encode Sans Thin",
-                   size = 4,
-                   nudge_y = 5,
-                   #nudge_x = 5,
+                   family = "Source Sans Pro Light",
+                   size = 4.5,
+                   nudge_y = 5.5,
+                   color = "#3FDAC4",
+                   nudge_x = ,
                    box.padding = 0.25,
                    segment.alpha = 0,
-                   label.size = NA) # remove border
+                   label.size = .25)
 
 ggsave(here::here("2020", "16_rap_songs", "All songs - artists.png"), device = "png", type = "cairo", width = 12, height = 10, dpi = 300)
 
@@ -78,41 +85,47 @@ ggsave(here::here("2020", "16_rap_songs", "All songs - artists.png"), device = "
 # Scatter plot of all of the songs and their total points - no artists
 
 # get the labels
-top <- rankings %>% 
-  head(6)
-
-notable <- rankings %>% 
-  filter(year >= 2010 | year == min(year)) %>% 
-  head(3)
-
-labels <- rbind(top,notable)
 
 rankings %>% 
   ggplot(aes(x = year, y = points)) +
   geom_point(aes(color = gender),size = 4.5, alpha = 0.3) +
+  geom_point(data = top_females, aes(x = year, y = points), color = "#f35588",size = 4, alpha = 0.3) +
   labs(title = "Top Hip Hop Songs",
        subtitle = "According to 107 critics worldwide",
        x = "",
        y = "Critic Rating",
        color = "",
-       caption = "Critic rating: Each critic voted for five songs, ranking them from one (favourite) to five (5th favourite).\nBBC Music awarded 10 points for first ranked track, eight points for second ranked track, \nand so on down to two points for fifth place.The song with the most points won.\nSource: BBC Music (2019)") +
+       caption = "Critic rating: Each critic voted for five songs, ranking them from one (favourite) to five (5th favourite). BBC Music awarded 10 points for first ranked track,\neight points for second ranked track, and so on down to two points for fifth place.The song with the most points won.\nSource: BBC Music (2019)") +
   scale_color_manual(values = c("#f35588","#05dfd7","#ffac41"),
                      labels = c("female rapper", "male rapper","mixed")) +
   theme(
-    plot.title = element_text(size = 30,),
+    plot.title = element_text(size = 30),
     plot.subtitle = element_text(size = 35),
+    plot.caption = element_text(size = 11),
     legend.position = c(.9, .9),
     legend.justification = c("right", "top"),
     legend.margin = margin(6, 6, 6, 6)
   ) +
-  geom_text_repel(data = labels, 
+  geom_label_repel(data = labels, 
                    aes(label = title),
-                   family = "Encode Sans Thin",
+                   family = "Source Sans Pro Light",
                    size = 4.5,
-                   nudge_y = 5,
+                   nudge_y = 5.5,
+                   color = "#3FDAC4",
                    nudge_x = ,
                    box.padding = 0.25,
-                   segment.alpha = 0)
+                   segment.alpha = 0,
+                   label.size = .25)  +
+  geom_label_repel(data = top_females, 
+                   aes(label = title),
+                   family = "Source Sans Pro Light",
+                   #fontface = "bold",
+                   size = 4.5,
+                   nudge_y = 5.5,
+                   color = "#f35588",
+                   box.padding = 0.2,
+                   segment.alpha = 0,
+                   label.size = .25) # remove border
 
 ggsave(here::here("2020", "16_rap_songs", "All songs.png"), device = "png", type = "cairo", width = 10, height = 9, dpi = 300)
 
@@ -132,3 +145,35 @@ polls %>%
   theme(axis.text.x = element_text(family = "Fira Mono",size = 12)) + coord_flip()
 
 ggsave(here::here("2020", "16_rap_songs", "Critics - countries.png"), device = "png", type = "cairo", width = 11, height = 4, dpi = 300)
+
+#----------------------------------------------------------------------------------------------
+# Animated plot (April 23, 2020)
+
+songs_animated <- rankings %>% 
+  ggplot(aes(x = year, y = points)) +
+  geom_point(aes(color = gender),size = 4, alpha = 0.3) +
+  labs(title = "Top Hip Hop Songs",
+       subtitle = "According to 107 critics worldwide",
+       caption = "Critic rating: Each critic voted for five songs, ranking them from one (favourite) to five (5th favourite).\nBBC Music awarded 10 points for first ranked track, eight points for second ranked track, \nand so on down to two points for fifth place.The song with the most points won.\nSource: BBC Music (2019)",
+       x = "",
+       y = "Critic Rating",
+       color = ""
+  ) +
+  scale_color_manual(values = c("#f35588","#05dfd7","#ffac41"),
+                     labels = c("female rapper", "male rapper","mixed")) +
+  theme(
+    plot.title = element_text(size = 28,),
+    plot.subtitle = element_text(size = 32),
+    plot.caption = element_text(size = 16),
+    legend.position = c(.85, .92),
+    legend.justification = c("right", "top"),
+    legend.margin = margin(6, 6, 6, 6),
+    legend.text = element_text("Gruppo", size = 22)
+  ) +
+  transition_states(gender,
+                    transition_length = 1,
+                    state_length = 2) +
+  enter_fade() + 
+  exit_shrink()
+animate(songs_animated, width = 800, height = 600)
+anim_save("All songs - animated.gif")
